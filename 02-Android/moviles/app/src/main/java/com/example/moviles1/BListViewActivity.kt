1093c186f295260e94a7e2576ba735aa4cb4f8b2
Jair Andres <jair.quinonez@epn.edu.ec>
2020-07-15 01:29:49 -0500

    ServicioBDDMemoria

---
+++ 02-Android/moviles/app/src/main/java/com/example/moviles1/BListViewActivity.kt
@@ -0,0 +1,36 @@
+package com.example.moviles1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_b_list_view.*
+
+class BListViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b_list_view)
+        val listEntrenador = arrayListOf<Entrenador>()
+        listEntrenador.add(Entrenador("Test","Testing"))
+        listEntrenador.add(Entrenador("Test2","Testing2"))
+        listEntrenador.add(Entrenador("Test3","Testing3"))
+        listEntrenador.add(Entrenador("Test4","Testing4"))
+        listEntrenador.add(Entrenador("Test5","Testing5"))
+
+        val adactador = ArrayAdapter(
+            this, //contexto
+            android.R.layout.simple_list_item_1, //nombre layaout
+            listEntrenador //Lista
+        )
+        lv_numeros.adapter = adactador
+        lv_numeros.onItemClickListener = AdapterView.OnItemClickListener{
+            parent, view, position, id -> Log.i("List-View", "Position $position")
+
+
+        }
+
+
+    }
+}
---
+++ 02-Android/moviles/app/src/main/java/com/example/moviles1/ServicioBDDMemoria.kt
@@ -0,0 +1,10 @@
+package com.example.moviles1
+
+class ServicioBDDMemoria {
+    companion object{
+        var numero = 0
+        fun anadir_numero(){
+            this.numero = this.numero + 1
+        }
+    }
+}
